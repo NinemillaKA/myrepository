2865cff62a0f5af706826429b7437e4d95954bba
Ninemilla Martins <ninemillaka@gmail.com>
2018-02-11 14:17:19 +0000

    Last Comited

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,7 +1,10 @@
-function printYouName(name){
+function printYouName(name, age){
     console.log("Type: "+name.typeof);
     console.log("Your Name is: "+name);
     console.log("Length:"+name.length)
+    console.log("Type: "+age.typeof);
+    console.log("Your Age is: "+age);
+    console.log("Length:"+age.length)
 }
 
 printYouName("Natalina");
